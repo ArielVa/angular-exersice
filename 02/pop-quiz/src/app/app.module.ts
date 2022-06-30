5dad067872e971183e99194dd63be08b95af6590
ArielVa <ariel_va@sii.org.il>
2022-06-30 09:28:54 +0300

    02 - Pop-quiz, initial setup

--- 02/pop-quiz/src/app/app.module.ts
+++ 02/pop-quiz/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { QuestionPresenterComponent } from './components/question-presenter/question-presenter.component';
+import { QuizOverComponent } from './components/quiz-over/quiz-over.component';
+import { SummaryComponent } from './components/summary/summary.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuestionPresenterComponent,
+    QuizOverComponent,
+    SummaryComponent
   ],
   imports: [
     BrowserModule
